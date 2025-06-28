da0b25412c42ad2c1bd4154d39aaa27321c4cece
hauntedcupoftea <andydchauhan@gmail.com>
2025-06-28 16:25:04 +0530

    astal/gtk4 is not found. need to fix.

--- app.ts
+++ app.ts
@@ -1,6 +1,6 @@
-import { App } from "astal/gtk4"
-import style from "./styles/main.scss"
-import Bar from "./widget/Bar"
+import { App } from "astal/gtk4";
+import style from "./styles/main.scss";
+import Bar from "./widget/Bar";
 
 App.start({
   css: style,
